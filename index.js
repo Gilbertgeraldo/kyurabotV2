270f63ae15743a4d277e4f1f75bb6fc8482776f5
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-15 18:43:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Rpg\nWhatsApp Bot', {
+CFonts.say('Rpg\nGilbert Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
